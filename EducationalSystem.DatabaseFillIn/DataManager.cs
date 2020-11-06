bca6984bf64f5118aaee530baaf6f1e1dc225d0d
ваше имя <zabenkotoi@gmail.com>
2020-11-06 09:38:36 +0300

    first version

---
+++ EducationalSystem.DatabaseFillIn/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using DatabaseStructure;
+using ServiceLayer.ServiceInterfaces;
+using ServiceLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalSystem.DatabaseFillIn
+{
+    public class DataManager
+    {
+        public ICourseService CoursesService { get; set; }
+
+        public IProfessorService ProfessorsService { get; set; }
+
+        public ISchoolService SchoolsService { get; set; }
+
+        public IStudentService StudentsService { get; set; }
+
+        public DataManager(DBContext dbContext)
+        {
+            CoursesService = new CourseService(dbContext);
+            ProfessorsService = new ProfessorService(dbContext);
+            SchoolsService = new SchoolService(dbContext);
+            StudentsService = new StudentService(dbContext);
+        }
+    }
+}
---
+++ EducationalSystem.DatabaseFillIn/Program.cs
@@ -0,0 +1,33 @@
+﻿using DatabaseStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalSystem.DatabaseFillIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataManager dataManager;
+            using (DBContext context = new DBContext())
+            {
+                dataManager = new DataManager(context);
+                try
+                {
+                    dataManager.CoursesService.AddCourse("OOOP", 123, true);
+                    dataManager.ProfessorsService.AddProfessor("Lia", "Silver", "liasilver@mail.ru", "+375292477096", true);
+                    dataManager.StudentsService.AddStudent("Lina", "Rusinovich", "lina.rusinovichr@mail.ru", "+375293245096", true);
+                    dataManager.SchoolsService.AddSchool("School n5");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }               
+            }
+            Console.ReadLine();
+        }
+    }
+}
